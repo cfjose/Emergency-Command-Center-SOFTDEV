089df7235db28aa12a9b17dcf67526f1364c56d3
edricaranto <edric.caranto@gmail.com>
2017-03-02 19:15:33 +0800

    fix forms classes for registration

--- Application/Emergency-Command-Center/gentelella/core/views.py
+++ Application/Emergency-Command-Center/gentelella/core/views.py
@@ -33,7 +33,23 @@ def SettingsPageView(request):
     return render(request, 'settings.html')
 
 def RegisterPageView(request):
-    return render(request, 'admin/register_user.html')
+    #return render(request, 'admin/register_user.html')
+    # if this is a POST request we need to process the form data
+    if request.method == 'POST':
+        # create a form instance and populate it with data from the request:
+        form = Register(request.POST)
+        # check whether it's valid:
+        if form.is_valid():
+            # process the data in form.cleaned_data as required
+            # ...
+            # redirect to a new URL:
+            return HttpResponseRedirect('/thanks/')
+
+    # if a GET (or any other method) we'll create a blank form
+    else:
+        form = Register()
+
+    return render(request, 'admin/register_user.html', {'form': form})
 
 def GetRegistration(request):
     # if this is a POST request we need to process the form data
