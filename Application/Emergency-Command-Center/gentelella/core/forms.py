ee20f50f12bf0056843f6054bfb1e242ad548ed6
cfjose <chamberj18@gmail.com>
2017-03-28 15:01:20 +0800

    Changelog(s):      - Replaced Django framework template with CodeIgniter

--- Application/Emergency-Command-Center/gentelella/core/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-
-class Register(forms.Form):
-    fname = forms.CharField(label='First name', max_length=50)
-    lname = forms.CharField(label='Last name', max_length=50)
-    uname = forms.CharField(label='Username', max_length=50)
-    email = forms.EmailField(label='Email', max_length=100)
-    pword = forms.CharField(label='Password', widget=forms.PasswordInput(attrs=None ,render_value=True))
